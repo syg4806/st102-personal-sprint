754b4bc88ff43c348797862b3fbe2f0ff264e54e
rkdmf1026 <rkdmf1026@naver.com>
2020-11-21 14:02:36 +0900

    Fix TwoPickPlus.java

--- CodingTest/dglee/TwoPickPlus.java
+++ CodingTest/dglee/TwoPickPlus.java
@@ -4,26 +4,19 @@ import java.util.*;
 public class TwoPickPlus {
 	 public int[] solution(int[] numbers) {
 		 int[] answer;
-	        
-	        List<Integer> list = new ArrayList<Integer>();
-	        
-	        for(int index1=0;index1<numbers.length;index1++) {
-	        	for(int index2=0;index2<numbers.length;index2++) {
-	        		if(index1 != index2) {
-	        			int insert = numbers[index1] +numbers[index2];
-	        			if(list.indexOf(insert) ==-1) 
-	        			list.add(insert);	
-	        		}
-	        		
-	        	}
-	        }
-	        Collections.sort(list);
-	        answer = new int[list.size()];
-	        
-	        for(int i=0;i<list.size();i++) {
-	        	answer[i]=list.get(i);
-	        }
-	       
-	        return answer;
-	    }
+		 HashSet<Integer> set = new HashSet<Integer>();
+
+		 for(int index1=0;index1<numbers.length;index1++) {
+			 for(int index2=0;index2<numbers.length;index2++) {
+				 if(index1 != index2)
+					 set.add(numbers[index1] +numbers[index2]);			
+			     }
+			 }
+		 answer = new int[set.size()];
+		 int i=0;
+		 for(int index : set)
+			 answer[i++] = index;
+		 Arrays.sort(answer);
+		 return answer;
+		}
 }
