20b17019336c4251baca81412686534871c19711
myeongheonhong <70846061+myeongheonhong@users.noreply.github.com>
2021-01-27 13:24:42 +0900

    Update ex4.java
    문제를 바꿔서 다시짰던코드가 저장을 안하고 커밋, 푸쉬를 해서 그런지 바꾸기 전 문제 코드가 올라갔네요..

--- CodingTest/hongmh/2021_01_3rd/src/ex/ex4.java
+++ CodingTest/hongmh/2021_01_3rd/src/ex/ex4.java
@@ -1,38 +1,14 @@
 package ex;
 
-import java.util.*;
+//문제링크 : https://programmers.co.kr/learn/courses/30/lessons/12937
 
 class Solution {
-    public String solution(int n) {
-        String answer = "";
-        Stack<String> keep = new Stack<>();
-        
-        
-        while(n > 3) {
-        	if(n%3 == 0)
-            	keep.push(Integer.toString(4));
-            else if(n%3 == 1)
-            	keep.push(Integer.toString(1));
-            else if(n%3 == 2)
-            	keep.push(Integer.toString(2));
-        	
-        	n/=3;
-        }
-        
-        if(n == 3)
-        	keep.push(Integer.toString(4));
-        else if(n == 2)
-        	keep.push(Integer.toString(2));
-        else if(n == 1) 
-        	keep.push(Integer.toString(1));
-        
-        while(!keep.isEmpty()) {
-        	answer+=keep.peek();
-        	keep.pop();
-        }
-        	
-        	
-        return answer;
+    public String solution(int num) {
+        if(num%2 == 0)
+        	return "Even";
+        else
+        	return "Odd";
+       
     }
 }
 public class ex4 {
